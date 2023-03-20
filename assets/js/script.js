27eca7ba565da6b5aaecd2c0b89bcbf17ea658db
raffaele-catalano <raf.catalano@gmail.com>
2023-03-20 20:02:19 +0100

    styled elements + added js elementsById

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,10 @@ const age = prompt('Inserisci la tua età', '33');
 const distance = prompt('Inserisci la distanza che intendi percorrere in Km', '30');
     console.log(firstName, surname, age, distance);
 
+document.getElementById('anagraphic').innerHTML = firstName + " " + surname;
+document.getElementById('age').innerHTML = "Età:" + " " + age
+document.getElementById('distance_km').innerHTML = "Distanza:" + " " + distance + " " + "Km" 
+
 let prezzoFinale = distance * 0.21;
 
 if (age < 18) {
@@ -14,5 +18,6 @@ if (age < 18) {
 
 console.warn(prezzoFinale);
 
+document.getElementById('final_price').innerHTML = "Prezzo del ticket:" + " " + "€" + " " + prezzoFinale.toFixed(2);
 
-document.getElementById('final-price').innerHTML = prezzoFinale.toFixed(2);
+// document.getElementById('final-price').innerHTML = prezzoFinale.toFixed(2);
