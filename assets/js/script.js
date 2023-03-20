ecac00255906ad69bcf59027db18c8a361e0f3a0
raffaele-catalano <raf.catalano@gmail.com>
2023-03-20 19:38:32 +0100

    start style.css aestethic

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,17 @@ const firstName = prompt('Inserisci il tuo Nome', 'Nome');
 const surname = prompt('Inserisci il tuo Cognome', 'Cognome');
 const age = prompt('Inserisci la tua età', '33');
 const distance = prompt('Inserisci la distanza che intendi percorrere in Km', '30');
+    console.log(firstName, surname, age, distance);
 
-let finalPrice = distance * 0.21;
+let prezzoFinale = distance * 0.21;
 
 if (age < 18) {
-    finalPrice = finalPrice * 0.8;
+    prezzoFinale = prezzoFinale * 0.8;
 } else if (age > 65) {
-    finalPrice = finalPrice * 0.6;
+    prezzoFinale = prezzoFinale * 0.6;
 }
 
-document.getElementById('final-price').innerText = finalPrice.toFixed(2);
+console.warn(prezzoFinale);
+
+
+document.getElementById('final-price').innerHTML = prezzoFinale.toFixed(2);
